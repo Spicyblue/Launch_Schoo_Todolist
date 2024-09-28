ec85593bb98240a81e5ed40f0c35183fbd770eb6
ochuloro <ochulor.okechukwu@gmail.com>
2024-09-28 18:07:17 +0200

    python script necessary for the todolist program

--- todolist.py
+++ todolist.py
@@ -1,41 +1,4 @@
-'''
-The Todo List mini application
-'''
-class Todo:
-    IS_DONE = 'X'
-    IS_UNDONE = ' '
-
-    def __init__(self, title):
-        self._title = title
-        self._done = False
-
-    @property
-    def title(self):
-        return self._title
-    
-    @title.setter
-    def title(self, title):
-        self._title = title
-    
-    @property
-    def done(self):
-        return self._done
-    
-    @done.setter
-    def done(self, done):
-        self._done = done
-
-    def __eq__(self, other):
-        if not isinstance(other, Todo):
-            return NotImplemented
-        
-        return (self.title == other.title) and (self.done == other.done)
-
-    def __str__(self):
-        if self.done:
-            return f"[{self.IS_DONE}] {self._title}"
-        
-        return f"[{self.IS_UNDONE}] {self._title}"
+from todo import Todo
 
 class TodoList:
     def __init__(self, title):
@@ -428,7 +391,7 @@ def step_12():
     def y_in_title(todo):
         return 'y' in todo.title
 
-    print(todo_list.select(lambda todo: y_in_title))
+    print(todo_list.select(lambda todo: y_in_title(todo)))
     # ---- Today's Todos -----
     # [ ] Buy milk
     # [ ] Go to gym
@@ -536,4 +499,7 @@ def main():
     step_14()
     step_15()
 
-main()
+if __name__ == "__main__":
+    print('Starting Todolist app \n')
+    main()
+    print('Ending Todolist app')
