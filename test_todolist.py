0bedf8d65fb7fb7bc1c0573b4a51fb142e22e40a
ochuloro <ochulor.okechukwu@gmail.com>
2024-09-28 18:11:19 +0200

    Test script for testing the todolist app

---
+++ test_todolist.py
@@ -0,0 +1,23 @@
+import unittest
+from todolist import Todo, TodoList
+
+class TestTodoList(unittest.TestCase):
+    def setUp(self):
+        self.todo1 = Todo("Buy milk")
+        self.todo2 = Todo("Clean room")
+        self.todo3 = Todo("Go to the gym")
+
+        self.todos = TodoList("Today's Todos")
+        self.todos.add(self.todo1)
+        self.todos.add(self.todo2)
+        self.todos.add(self.todo3)
+
+    def test_length(self):
+        self.assertEqual(3, len(self.todos), "Not exactly 3 todos in your list")
+
+if __name__ == "__main__":
+    print('Test starting')
+    try:
+        unittest.main()
+    finally:
+        print('Test ended')
